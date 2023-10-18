a3a402885d30667f4a0cc3e6fa6580b789da0999
Do Tran <do.tran@saigontechnology.com>
2023-10-18 14:34:42 +0700

    add deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: deno()
 });
