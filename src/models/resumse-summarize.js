16dbaa5c1f41ab7454554a196986af8a7cee4e2b
do.tran <do.tran@saigontechnology.com>
2025-03-21 16:59:07 +0700

    add resume to the shibuicat

---
+++ src/models/resumse-summarize.js
@@ -0,0 +1,135 @@
+export const ResumeSummary = {
+	name: "Tran Van Do",
+	workedSince: "May 01 2018",
+	title: ".NET Developer",
+	profilePicture: "profile-picture.jpg",
+	contact: {
+		address: "Ho Chi Minh, Viet Nam",
+		phone: "+84 774 276 286",
+		email: "tvddaman@gmail.com",
+		social: {
+			linkedIn: "tran-van-do",
+			github: "oldtune",
+			facebook: "",
+		},
+	},
+	education: [
+		{
+			school: "Cao Thang College",
+			major: "IT",
+		},
+	],
+	certificates: [
+		{
+			name: "AZ 900",
+			link: "",
+		},
+	],
+	skillSets: [
+		["C#", "Rust", "Javascript + Typescript", "HTML 5", "CSS 3"],
+		["SQL Server", "Postgre SQL", "Entity Framework"],
+		[".NET Framework & .NET Core", "Entity Framework & Entity Framework Core", "SignalR", "Angular", "React JS"],
+		["Linux", "Azure", "Docker"]
+	],
+	workHistories: [
+		{
+			name: "Sai Gon Technology",
+			link: "",
+			position: "Senior .NET Developer",
+			from: "May 2021",
+			to: "Now",
+			projects: [
+				{
+					name: "Idealpos",
+					description: "Cloud POS System which is widely used in Australian",
+					headCount: 6,
+					position: "Full stack developer",
+					mainResponsibility: "Develop new features, maintain old features, integrate with 3rd parties",
+					technologies:
+						[".NET Framework", ".NET Core", "EF Framework", "EF Core", "Azure SQL", "Angular", "Html", "Css", "SignalR & WebSocket", "Stripe & Adyen payment"],
+					additionalInfo: "",
+				},
+			],
+		},
+		{
+			name: "Relationshop",
+			link: "",
+			position: "Junior .NET Developer",
+			from: "May 2020",
+			to: "May 2021",
+			projects: [
+				{
+					name: "ECommerce Platform",
+					description: "ECommerce platform project for multiple clients based in US",
+					headCount: 20,
+					position: ".NET developer",
+					mainResponsibility: "Develop new features, maintain old features",
+					technologies:
+						[".NET Framework & .NET Core", "EF Framework & EF Core", "SQL Server", "Jenkins & On premise hosting"],
+					additionalInfo: "",
+				},
+			],
+		},
+		{
+			name: "New Ocean Infosys",
+			link: "",
+			position: "Junior full stack Developer",
+			from: "Jan 2019",
+			to: "May 2020",
+			projects: [
+				{
+					name: "Cement factory management",
+					description: "Inventory & Warehouse management, factory operations management",
+					headCount: 8,
+					position: "Full stack developer",
+					mainResponsibility: "Develop project from scratch, fix bugs and report to team leader",
+					technologies:
+						[".NET Framework", "EF Framework", "SQL Server", "SignalR", "Angular", "Html", "Css"],
+					additionalInfo: "",
+				},
+				{
+					name: "Insurance Project",
+					description: "Insurance project of an Australian client",
+					headCount: 4,
+					position: "Backend .NET developer",
+					mainResponsibility: "Maintain old features, fix bugs and write automation test using Selenium",
+					technologies:
+						[".NET Framework", "Web form", "Selenium", "Hibernate, Oracle"],
+					additionalInfo: "",
+				},
+			],
+		},
+		{
+			name: "CMC SISG",
+			link: "",
+			position: "Intern developer",
+			from: "May 2018",
+			to: "Jan 2019",
+			projects: [
+				{
+					name: "Resources management",
+					description: "Internal project that is used to manage resources, devices, invoices... within the company",
+					headCount: 12,
+					position: "Full stack developer",
+					mainResponsibility: "Develop new feature and maintain old features, then report to Team Leader",
+					technologies:
+						[".NET Framework", "EF Framework", "SQL Server", "AngularJS", "Html", "Css", "KendoUI"],
+					additionalInfo: "",
+				},
+			],
+		},
+	],
+	sideProjects: [
+		{
+			name: "Shibuicat Universe",
+			link: "",
+			description: "It's a framework for self development",
+			headCount: 1,
+			position: "Solo developer",
+			mainResponsibility: "Develop/ maintain all the features",
+			technologies:
+				[".NET Core", "EF Core", "AstroJS", "PostgreSQL", "Rust", "Self Hosting", "AWS"],
+			additionalInfo: "",
+		},
+	],
+};
